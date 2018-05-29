bb35eb6d3d4eba96ab6a48440bc687a0687624bc
caodonnell <caodonnell@email.arizona.edu>
2018-05-29 13:35:22 -0700

    greyscale bins initial commit

--- allfuncs.py
+++ allfuncs.py
@@ -4,14 +4,21 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 
+#ignore image size warnings
+import warnings
+#warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore", message="Image size")
+
 def testfunc(n):
     print(n)
     return
 
+
 #scale and img width/height have to be in same distance units
 #in this case, miles
-def resize(newscale, imgname='lestermadeline_BW.jpg', img_width=2600, img_height=1600, fig_width=12, fig_height=10):
-    img = Image.open(imagename)
+#NEED TO PUT MIN/MAX CONTROLS
+def resize(newscale, img_name='lestermadeline_BW.jpg', img_width=2600, img_height=1600, fig_width=12, fig_height=10):
+    img = Image.open(img_name)
     px_width, px_height = img.size
     
     widthscale = 1.0*img_width/px_width
@@ -31,3 +38,28 @@ def resize(newscale, imgname='lestermadeline_BW.jpg', img_width=2600, img_height
     plt.show()
     
     return 
+
+#function to help with rebinning a grey scale image
+def rebin_func(value, imgscale):
+    return np.where(imgscale <= value)[0][-1]
+
+#NEED TO ADD MIN/MAX CONTROL
+def rebin(nbins, img_name='lestermadeline_BW_5mile.jpg', fig_width=12, fig_height=10):
+    img = Image.open(img_name)
+    imgarray = np.asarray(img)
+    
+    imgmin = np.min(imgarray)
+    imgmax = np.max(imgarray)
+    imgscale = np.linspace(imgmin, imgmax, nbins+1)
+    new_img = [[rebin_func(val, imgscale) for val in x] for x in imgarray]
+    
+    plt.rcParams['figure.figsize'] = [fig_width, fig_height]
+    fig = plt.figure()
+
+    imshow(new_img, cmap='gray')
+    plt.axis('off')
+    
+    plt.show()
+    
+    return 
+    
